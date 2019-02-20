9c3537fc5ef563233acb16740102a6dc035b8c8d
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2019-02-20 12:55:57 +0530

    Update 92.c

--- 92.c
+++ 92.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	 int no[5],i,sum=0;
+	 int num[5],i,sum=0;
 	 for(i=0;i<5;i++)
 	 {
-	 scanf("%d",&no[i]);
+	 scanf("%d",&num[i]);
 	 }
 	for(i=0;i<5;i++)
 	 {
-	 sum=sum+no[i];
+	 sum=sum+num[i];
 	 }
 	 printf(" %d",sum);
 	 
