441c278d0c63760da7a2cb2fe02aa9dca5db971a
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2019-02-20 12:24:44 +0530

    Update 96.py

--- 96.py
+++ 96.py
@@ -1,10 +1,10 @@
-m=int(raw_input())
-c=0
+k=int(raw_input())
+s=0
 if m>1:
-    for i in range(2,m):
+    for i in range(2,k):
         if m%i==0:
-            c=c+1
-if c>1:
+            s=s+1
+if s>1:
    print "yes"
 else:
     print "no"
