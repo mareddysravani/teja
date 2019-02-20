d09b971ab8967e617af5818da2eb8e1bee92ae6f
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2019-02-20 12:46:12 +0530

    Update and rename 100.py to 100.c

--- 100.c
+++ 100.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void) {
-	int n,a,product=1;
+	int r,a,product=1;
 	scanf("%d",&n);
-	while(n!=0)
+	while(r!=0)
 	{
-		a=n%10;
+		a=r%10;
 		product=product*a;
-		n=n/10;
+		r=r/10;
 	}
 	printf("%d",product);
 	return 0;
