eedca44e15201a0a75c59985da40c60582bcf0f1
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2019-02-20 12:30:14 +0530

    Update 97.py

--- 97.py
+++ 97.py
@@ -1,7 +1,7 @@
-n=int(raw_input())    
+m=int(raw_input())    
 Reverse=0    
-while(n>0):    
-    Reminder=n%10    
+while(m>0):    
+    Reminder=m%10    
     Reverse=(Reverse*10)+Reminder    
-    n=n//10    
+    m=m//10    
 print(Reverse)   
