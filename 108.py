cba8c323ec4d1563e348ec38e4c0f981ce8a4532
mareddysravani <42571301+mareddysravani@users.noreply.github.com>
2019-02-23 22:44:27 +0530

    Update 108.py

--- 108.py
+++ 108.py
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main()
 {
- int a,d,n,s;
- scanf("%d %d %d",&a,&d,&n);
- s=(n/2)*(2*a+(n-1)*d);
+ int a,d,r,s;
+ scanf("%d %d %d",&a,&d,&r);
+ s=(r/2)*(2*a+(r-1)*d);
 printf("%d",s);
 }
